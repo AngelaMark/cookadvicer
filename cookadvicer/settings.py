8c9d467d8aa1120baf3bb073478bd2785dd97c1e
Angela <AngelaMark@users.noreply.github.com>
2016-08-30 16:16:48 +0400

    Update settings.py

--- cookadvicer/settings.py
+++ cookadvicer/settings.py
@@ -125,5 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
