af2490933a00e4bc33045fc423a584db4f51fb0c
AngelaMark <angelamarkosyan95@gmail.com>
2016-08-30 18:54:09 +0400

    settings change

--- cookadvicer/settings.py
+++ cookadvicer/settings.py
@@ -12,12 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
-
-<<<<<<< HEAD
 from os import path
 
-=======
->>>>>>> origin/master
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,6 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
 PROJECT_ROOT = os.path.dirname(__file__)
 STATIC_ROOT =path.join(PROJECT_ROOT,'static/')
 STATIC_URL =path.join(PROJECT_ROOT,'static/')
