3be7e76dfacf51c033e4e1c9d60d93deb0c1cdaa
AngelaMark <angelamarkosyan95@gmail.com>
2016-08-30 18:50:06 +0400

    settings change

--- fb_cookbot/urls.py
+++ fb_cookbot/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import  url
 from .views import Cooker
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^66d2b8f4a09cd35cb23076a1da5d51529136a3373fd570b122/?$', Cooker.as_view())
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
